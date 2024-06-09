d571b45929cda4fe70323caf9b6b73857793c1e3
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-09 08:51:59 +0300

    delete blurhash

--- src/components/main/viewer/Viewer.tsx
+++ src/components/main/viewer/Viewer.tsx
@@ -1,7 +1,6 @@
 import styles from "./Viewer.module.css";
 import Button from "../../UI/button/Button";
 import { mainData } from "../../../util/interfaces";
-import ImageComponent from "../../UI/ImageComp/ImageComponent";
 const Viewer: React.FC<mainData> = (props) => {
   return (
     <div
@@ -10,7 +9,7 @@ const Viewer: React.FC<mainData> = (props) => {
       style={{ fontFamily: "Tajawal" }}
     >
       <div className={styles.viewer__shadow} />
-      <ImageComponent
+      <img
         src={props.img}
         className={styles.viewer__background}
       />
