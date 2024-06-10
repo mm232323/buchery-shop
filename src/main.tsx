583689605dae7742739f223cebe0be15db895dc8
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-10 17:41:40 +0300

    add speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import {  QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import store from "../store/index";
 import { queryClient } from "./util/http.ts";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 const Loading = lazy(() => import('./loadingPage/Loading.tsx'))
 const root = document.getElementById("root");
 ReactDOM.createRoot(root!).render(
@@ -14,7 +13,6 @@ ReactDOM.createRoot(root!).render(
     <Provider store={store}>
     <Suspense fallback={<Loading />}>
       <App />
-      <SpeedInsights />
     </Suspense>
     </Provider>
   </QueryClientProvider>
