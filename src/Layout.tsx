5193039a05af67b350e625268fa916427993be13
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-10 17:49:13 +0300

    remove vercel speed

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,8 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Outlet } from "react-router";
 import Footer from "./components/layout/footer/Footer";
 export default function MainLayout() {
   return (
     <>
-      <SpeedInsights />
       <Outlet />
       <Footer />
     </>
