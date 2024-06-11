04a31e4dcc3d6064adc195056000eb2147e82df1
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-11 18:47:53 +0300

    fixing styles issues

--- src/pages/loadingPage/Loading.tsx
+++ src/pages/loadingPage/Loading.tsx
@@ -1,11 +1,12 @@
 import LoadingImg from "/loading.gif";
 import styles from "./Loading.module.css";
-export default function Loading() {
+const Loading:React.FC<{top?:number}> = ({top}) => {
   return (
     <div className={styles.loadingP__container}>
       <center>
-        <img className={styles.LoadingGif} src={LoadingImg} alt="loading img" />
+        <img  style={{top:(top!) + 'rem'}} className={styles.LoadingGif} src={LoadingImg} alt="loading img" />
       </center>
     </div>
   );
 }
+export default Loading
--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -3,14 +3,21 @@ import titleP from "/mainTitle.png";
 import styles from "./Start.module.css";
 import { Link } from "react-router-dom";
 import Logo from "/logo.png";
+import { lazy, useState } from "react";
+const  Loading =  lazy(() => import("../loadingPage/Loading"))
 export default function StartingPage() {
+  const [pathLoading,setPathLoading] = useState(false)
+  function handleLinkText() {
+    setPathLoading(true)
+  }
+  if (pathLoading) return <Loading top={25} />
   return (
     <div className={styles.startPage}>
       <img src={background} className={styles.back} alt="background" />
       <div className={styles.container} style={{ fontFamily: "Tajawal" }}>
         <img src={titleP} className={styles.title} alt="title photo" />
         <Link to="/main">
-          <button className={styles.but}>اضغط للبدأ</button>
+          <button onClick={handleLinkText} className={styles.but}>اضغط للبدء</button>
         </Link>
         <img src={Logo} className={styles.logo} alt="logo" />
       </div>
