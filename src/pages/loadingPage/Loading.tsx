357924f8daac84a03bca3d5d5dbf1334256fde6f
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-13 23:02:49 +0300

    updating motion

--- src/pages/loadingPage/Loading.tsx
+++ src/pages/loadingPage/Loading.tsx
@@ -5,7 +5,7 @@ const Loading:React.FC<{top?:number}> = ({top}) => {
   return (
     <div className={styles.loadingP__container}>
       <center>
-        <motion.img  style={{top:(top!) + 'rem'}} className={styles.LoadingGif} src={LoadingImg} alt="loading img" />
+        <motion.img animate={{opacity:1,scale:1}} exit={{opacity:0,scale:0}} style={{top:(top!) + 'rem'}} className={styles.LoadingGif} src={LoadingImg} alt="loading img" />
       </center>
     </div>
   );
