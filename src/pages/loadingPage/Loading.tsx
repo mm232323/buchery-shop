8ce4d5d4b63f2ad3f6c3d338700ecfc3f3dceb29
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-06-13 22:55:37 +0300

    adding motion

--- src/pages/loadingPage/Loading.tsx
+++ src/pages/loadingPage/Loading.tsx
@@ -1,10 +1,11 @@
 import LoadingImg from "/loading.gif";
 import styles from "./Loading.module.css";
+import {motion} from 'framer-motion'
 const Loading:React.FC<{top?:number}> = ({top}) => {
   return (
     <div className={styles.loadingP__container}>
       <center>
-        <img  style={{top:(top!) + 'rem'}} className={styles.LoadingGif} src={LoadingImg} alt="loading img" />
+        <motion.img  style={{top:(top!) + 'rem'}} className={styles.LoadingGif} src={LoadingImg} alt="loading img" />
       </center>
     </div>
   );
