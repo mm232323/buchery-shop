012046e027727416a498f5de914842704c317fe9
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-07-31 08:34:03 +0300

    fixing the loading component

--- src/pages/loadingPage/Loading.tsx
+++ src/pages/loadingPage/Loading.tsx
@@ -1,13 +1,10 @@
 import LoadingImg from "/loading.gif";
 import styles from "./Loading.module.css";
-import { motion } from "framer-motion";
 const Loading: React.FC<{ top?: number }> = ({ top }) => {
   return (
     <div className={styles.loadingP__container}>
       <center>
-        <motion.img
-          animate={{ opacity: 1, translateY: 0 }}
-          exit={{ opacity: 0, translateY: 30 }}
+        <img
           style={{ top: top! + "rem" }}
           className={styles.LoadingGif}
           src={LoadingImg}
